ce0b6b971e5e8c58d4eb908708de48abef64d153
Vaishali <vaishali.bariwal@gmail.com>
2019-06-30 09:07:12 +0530

    Ramdom pattern

---
+++ Basic_Programming/BitManipulation/BasicOfBitManipulation/14:Mystery/Solution.java
@@ -0,0 +1,55 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.BufferedWriter;
+import java.io.OutputStreamWriter;
+
+class TestClass {
+    public static void main(String args[] ) throws Exception {
+        
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+        String nstr;
+        while(true)
+        {
+            nstr = br.readLine();
+            if(nstr.length() != 0)
+            {
+                System.out.println("kkk"+nstr);
+                long n = Long.parseLong(nstr);
+                long count = 0;
+                if(isPowerOfTwo(n))
+                {
+                    count = n;
+                    sb.append(count+"\n");
+                }
+                else
+                {
+                    count = check(n);
+                    count = count == -1?0:(long)Math.pow(2, (long)count);
+                    sb.append(count+"\n");
+                }
+
+            }
+            else
+            {
+                System.out.println(sb);
+                System.exit(0);
+            }
+        }
+    }
+    static boolean isPowerOfTwo(long x)
+    {
+        // x will check if x == 0 and !(x & (x - 1)) will check if x is a power of 2 or not
+        return (x == 1 && (x & (x - 1)) == 0);
+    } 
+    static long check (long N)
+    {
+        int s = Long.SIZE;
+        for(int i = 0;i < s;i++)
+        {
+            if( (N & (1 << i)) > 0 )
+                return i;
+        }
+        return -1;
+    }
+}
---
+++ Basic_Programming/BitManipulation/BasicOfBitManipulation/14:Mystery/readme.md
@@ -0,0 +1,26 @@
+Jon Snow visits Dragonstone to meet Daenerys Targaryen. He asks for aid in defeating the White Walkers and Army of Dead. But Daenerys refuses to believe that white walkers are real. She puts a condition before Jon that if he solves the challenge given by her then she will send her army to fight White Walkers. She gives certain inputs and outputs , Jon needs to find the logic and predict the output for the corresponding inputs. Jon Snow is struggling with the challenge as he knows nothing!! Help Jon to find the logic and win this challenge.
+
+INPUT : Input contains integers separated by a newline
+
+OUTPUT : For the given input , Output the required answer.
+
+NOTE : No. of input integers are unknown.
+
+CONSTRAINTS :
+
+0<= N (total inputs)<= 10^15
+
+SAMPLE INPUT 
+0
+1
+5
+12
+22
+1424
+SAMPLE OUTPUT 
+0
+1
+1
+4
+2
+16
