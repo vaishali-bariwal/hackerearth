6de9cd87844c87ab63241ccfd470a8409ac4385f
Vaishali <vaishali.bariwal@gmail.com>
2019-07-11 19:22:05 +0530

    easy sum set problem

---
+++ Algorithm/Searching/linearSearch/2:easySumsetProblem/Solution.java
@@ -0,0 +1,45 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.*;
+
+class TestClass {
+    public static void main(String args[] ) throws Exception {
+       
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();                // Reading input from STDIN
+        int n = Integer.parseInt(br.readLine());
+        String[] arrA = br.readLine().split(" ");
+        int a[] = new int[n];
+        for(int i = 0;i < n;i++)
+        {
+            a[i] = Integer.parseInt(arrA[i]);
+        }
+        int m = Integer.parseInt(br.readLine());
+        String[] arrC = br.readLine().split(" ");
+        int c[] = new int[m];
+        for(int i = 0;i < m;i++)
+        {
+            c[i] = Integer.parseInt(arrC[i]);
+        }
+        Arrays.sort(a);
+        Arrays.sort(c);
+        for(int i=1;i<=100;i++)
+          {
+              int count=0;
+              for(int j=0;j<n;j++)
+              {
+                  for(int k=0;k<m;k++)
+                  {
+                      if(a[j]+i==c[k])
+                      {
+                          count++;
+                          break;
+                      }
+                  }
+              }
+              if(count==n)
+              sb.append(i+" ");
+          }
+        System.out.println(sb);
+    }
+}
