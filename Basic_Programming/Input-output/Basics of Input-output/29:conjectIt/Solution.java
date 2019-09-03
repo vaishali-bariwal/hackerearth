9118921bca861124c694d78c42f2f0418a35b260
Vaishali <vaishali.bariwal@gmail.com>
2019-09-03 23:46:54 +0530

    conject it

---
+++ Basic_Programming/Input-output/Basics of Input-output/29:conjectIt/Solution.java
@@ -0,0 +1,13 @@
+import java.util.*;
+class TestClass {
+    public static void main(String args[] ) {
+        Scanner sc = new Scanner(System.in);
+        int t = sc.nextInt(); 
+        sc.nextLine();
+        while(t > 0) {
+            String n = sc.nextLine();   
+            System.out.println("YES");  
+            t --;
+        }
+    }
+}
